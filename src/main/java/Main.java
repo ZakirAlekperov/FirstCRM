e17a08706978d3e460331f3fd61a18e652f2619b
zakiralekperov <zakiralekperov1998@gmail.com>
2023-06-18 21:08:27 +0300

    Добавлен запуск диалогового окна

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import view.Add;
+
+/**
+ * CRM система на Java Swing
+ * @author ZakAlek
+ * @version 0.1
+ */
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Add dialog = new Add();
+            dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            dialog.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
